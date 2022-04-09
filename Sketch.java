599f2b3d55b9bd1b12bb700b7154d3cb595e086c
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2022-04-09 15:12:43 -0400

    finished lvl 4 stuff

--- Sketch.java
+++ Sketch.java
@@ -27,26 +27,73 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	drawFace(200,40);
+  drawFace2(100,50, 56, 255);
+  getLocation(width/2, height/2);
      
   }
 
-  private void drawFace(int a, int b){
+ /**
+  * Parameters to draw the face
+  * @param x Determines the value for the x axis
+  * @param y Determines the value for the y axis
+  */
+  private void drawFace(int x, int y){
 
   //face
   fill(255,255,255);
-  ellipse(a, b, width/5, height/5); 
+  ellipse(x, y, width/5, height/5); 
 
   //Left hole
   fill(0,0,0);
-  ellipse(a-20,b-20, width/40, height/40);
+  ellipse(x-20,y-20, width/40, height/40);
 
   //Right hole
   fill(0,0,0);
-  ellipse(a+20, b-20, width/40, height/40); 
-
-  line(a-20,b+10,a+20,b+15);
+  ellipse(x+20, y-20, width/40, height/40); 
+  
+  stroke(0,0,0);
+  line(x-20,y+10,x+20,y+15);
 
   }
+  /**
+   * Parameters for the second face
+   * @param x2 Determines the value for the x axis of the second face
+   * @param y2 Determines the value for the y axis of the second face
+   * @param faceColour Determines the colour of the face
+   * @param eye Determines the colour of the eye and mouth
+   */
+  private void drawFace2(int x2, int y2, int faceColour, int eye){
+  //face
+  fill(faceColour,faceColour,faceColour);
+  ellipse(x2, y2, width/5, height/5); 
   
+  //Left hole
+  fill(eye,eye,eye);
+  ellipse(x2-20,y2-20, width/40, height/40);
+  
+  //Right hole
+  fill(eye,eye,eye);
+  ellipse(x2+20, y2-20, width/40, height/40); 
+  
+  stroke(eye,eye,eye);
+  line(x2-10,y2+20,x2+15,y2+20);
+  
+  }
+
+  /**
+   * Parameters for the red circle
+   * @param placeX Get the location of the X Value
+   * @param placeY Gets the location of the Y value
+   * @return returns X and Y value
+   */
+  private int getLocation(int placeX, int placeY){  
+  
+    fill(255,0,0);
+    ellipse(placeX, placeY, 100, 100);
+
+    return placeX + placeY;
+
+  }
+
   // define other methods down here.
 }
